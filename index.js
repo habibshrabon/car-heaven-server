ac2a8a5bf964282938eab94be01393434e6a1fc3
habibshrabon <snhabib777@gmail.com>
2021-11-12 00:46:16 +0600

    server set up

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewzy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("car_shop");
+    const productCollection = database.collection("products");
+
+    // POST API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log("hit the post api", product);
+
+      const result = await productCollection.insertOne(product);
+      console.log(result);
+      res.json(result);
+    });
+
+    //GET Products API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //GET Single Product API
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Car Heaven Server is Running.");
+});
+
+app.listen(port, () => {
+  console.log("Server running at port", port);
+});
