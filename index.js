4ea8a38dd616fe4d5c691783b0c4a2c6d1514bdb
habibshrabon <snhabib777@gmail.com>
2021-11-12 17:07:19 +0600

    Delete api confiremed work

--- index.js
+++ index.js
@@ -21,6 +21,30 @@ async function run() {
     await client.connect();
     const database = client.db("car_shop");
     const productCollection = database.collection("products");
+    const orderCollection = database.collection("orders");
+    const reviewCollection = database.collection("review");
+
+    //GET API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //GET ORDERS API
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    //GET Single Product API
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
 
     // POST API
     app.post("/products", async (req, res) => {
@@ -32,19 +56,48 @@ async function run() {
       res.json(result);
     });
 
-    //GET Products API
-    app.get("/products", async (req, res) => {
-      const cursor = productCollection.find({});
-      const products = await cursor.toArray();
-      res.send(products);
+    //POST ADD ORDERS API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
     });
 
-    //GET Single Product API
-    app.get("/products/:id", async (req, res) => {
+    // GET single user order
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(query);
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+
+    //Post review
+    app.post("/review", async (req, res) => {
+      const product = req.body;
+
+      const result = await reviewCollection.insertOne(product);
+      console.log(result);
+      res.json(result);
+    });
+
+    //get review
+    app.get("/review", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(query);
+      const cursor = reviewCollection.find(query);
+      const review = await cursor.toArray();
+      res.json(review);
+    });
+
+    //DELETE API
+    app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const product = await productCollection.findOne(query);
-      res.json(product);
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
     });
   } finally {
     // await client.close();
